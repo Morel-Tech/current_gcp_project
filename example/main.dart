6c4363da3e11dd61e223ada2c0ba917ba323a02b
Marcus Twichel <developer@marcustwichel.com>
2023-10-20 10:18:10 -0600

    chore: upgrade deps

--- example/main.dart
+++ example/main.dart
@@ -1,6 +1,8 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:current_gcp_project/current_gcp_project.dart';
 
 void main() async {
-  final projectId = await CurrentProjectId().currentProjectId();
+  final projectId = await const CurrentGcpProject().currentProjectId();
   // use projectId...
 }
